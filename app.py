9376f71f1760c8645125f4d5a9a40ea08f0c839f
yinjiamayeverlyn <116322562+yinjiamayeverlyn@users.noreply.github.com>
2025-11-20 00:53:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -661,7 +661,9 @@ with tabs[3]:
     st.write("""
         For inquiries or collaboration, please contact:
 
-        - **Email:** <a href="mailto:everlynyinjiamay@gmail.com">everlynyinjiamay@gmail.com</a>  
+        - **Email:** <a href="mailto:everlynyinjiamay@gmail.com?subject=Deepfake Report&body=Hello, I have a question about the system.">
+                      everlynyinjiamay@gmail.com
+                    </a>
         - **GitHub:** [https://github.com/yinjiamayeverlyn/deepfake-video-detection](https://github.com/yinjiamayeverlyn/deepfake-video-detection)
         """, unsafe_allow_html=True)
 
@@ -671,3 +673,4 @@ with tabs[3]:
         © 2025 Deepfake Video Detection Web App | Developed for University Final Year Project 22004860
     </div>
     """, unsafe_allow_html=True)
+
