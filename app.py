483f05cdb5f5d7486ec40eac32be2d94fa47d981
yinjiamayeverlyn <116322562+yinjiamayeverlyn@users.noreply.github.com>
2026-04-01 11:57:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -295,7 +295,7 @@ if video_file:
                     'borderwidth': 1,
                     'bordercolor': '#ddd',
                     'threshold': {
-                        'line': {'color': color, 'width': 4},
+                        'line': {'color': "black", 'width': 4},
                         'thickness': 1.0,
                         'value': fake_prob
                     }
